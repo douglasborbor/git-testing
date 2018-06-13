e00cea80a413eeaabda13d6dcab506d8404792ff
dborbor <dborbor@hotelbeds.com>
2018-06-13 12:46:49 +0200

    Add description field to Attribute class

--- src/main/java/org/djbv/entity/Attribute.java
+++ src/main/java/org/djbv/entity/Attribute.java
@@ -3,9 +3,17 @@ package org.djbv.entity;
 public class Attribute<T> {
     private final String name;
     private final T value;
+    private final String description;
 
-    public Attribute(String name, T value) {
+    public Attribute(String name, T value, String description) {
         this.name = name;
         this.value = value;
+        this.description = description;
     }
+
+    public Attribute(String name, T value) {
+        this(name,value,null);
+    }
+
+
 }
